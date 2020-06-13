f594d9885790288fe5029caa40b78a69d0c32112
seybo <michaelatworkspace@gmail.com>
2020-06-13 10:39:59 +0530

    add my backend

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: "default",
+  defaultConnection: 'default',
   connections: {
     default: {
-      connector: "bookshelf",
+      connector: 'bookshelf',
       settings: {
-        client: "sqlite",
-        filename: env("DATABASE_FILENAME", ".tmp/data.db")
+        client: 'postgres',
+        host: env('DATABASE_HOST', '127.0.0.1'),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', 'axel'),
+        username: env('DATABASE_USERNAME', 'glaux'),
+        password: env('DATABASE_PASSWORD', ''),
+        ssl: env.bool('DATABASE_SSL', false),
       },
       options: {
-        useNullAsDefault: true
+        ssl: false,
       }
-    }
-  }
+    },
+  },
 });
